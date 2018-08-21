8300c309f8adfd9d6edd918fe43d09fa13a3ff76
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2018-08-21 12:17:57 +0100

    v2

--- rss.js
+++ rss.js
@@ -1,117 +1,208 @@
-var feedsLibrary = {
-  "Engadget": "http://www.engadget.com/rss-full.xml",
-  "Hacker News": "http://news.ycombinator.com/rss",
-  "Adafruit": "http://www.adafruit.com/blog/feed/",
-  "Hackaday": "http://www.hackaday.com/rss.xml",
-  "The Verge": "http://www.theverge.com/rss/index.xml",
-  "9to5Mac": "http://9to5mac.com/feed/",
-  "MacRumors": "http://www.macrumors.com/macrumors.xml",
-  "GSM Arena": "https://www.gsmarena.com/rss-news-reviews.php3",
-  "A List Apart": "https://alistapart.com/main/feed"
-}
+/*
 
+Login
+https://winds-prod.getstream.io/auth/login
+base + "auth/login"
+
+Get Feeds
+https://winds-prod.getstream.io/follows?type=rss
+base + "follows?type=rss"
+
+Get Articles from all feeds
+https://winds-prod.getstream.io/users/<user_id>/feeds?page=1&per_page=10&type=article
+base + "users/" + id + "/feeds?page=0&per_page=10&type=article"
+
+Get Articles from feed
+https://winds-prod.getstream.io/articles?page=1&per_page=10&rss=<feed_id>&sort_by=publicationDate,desc
+base + "articles?page=1&per_page=10&rss=" + <feed_id> + "&sort_by=publicationDate,desc"
+
+Get Article content
+https://winds-prod.getstream.io/articles/<article_id>?type=parsed
+
+*/
+
+var base = "https://winds-prod.getstream.io/";
+var loading = false;
+
+var currentFeed = 0;
 var articlesElement = $(".articles");
-
-// $.each(feedsLibrary, function(feedName, feedUrl) {
-//   getFeed(feedName, feedUrl);
-// })
-var currentFeed = 0, feedName, feedUrl
-
+var articleElement = $(".article");
+var feedsElement = $(".feeds");
 currentFeed = parseInt(window.location.hash.substr(1)) || 0
 
 function init(){
-  articlesElement.html("")
-  window.location.hash = currentFeed
-  feedName = Object.keys(feedsLibrary)[currentFeed]
-  feedUrl = feedsLibrary[feedName]
-  getFeed(feedName, feedUrl)
-}
-init()
-
-window.previousFeed = function(e){
-  currentFeed = (currentFeed - 1)
-  if (currentFeed < 0) currentFeed = Object.keys(feedsLibrary).length-1
-  init()
-}
-window.nextFeed = function(e){
-  currentFeed = (currentFeed + 1) % Object.keys(feedsLibrary).length
-  init()
-}
-
-function getFeed(feedName, feedUrl){
-  var baseUrl = "https://query.yahooapis.com/v1/public/yql?q="
-  var queryString = encodeURI("SELECT * FROM feed WHERE url='" + feedUrl + "' LIMIT 10")
-  var format = "&format=json"
-  
-  var rssFeedPath = baseUrl + queryString + format
-  
-  $.getJSON(rssFeedPath, function(response) {
-    var feedItems = response.query.results.item
-    if(typeof feedItems == "undefined") feedItems = response.query.results.entry
-    var titleBar = $('<div class="feed-title">'+feedName+'</div>')
-    var itemSource = $('<p class="feed-source">' + feedName + '</p>')
-    articlesElement.append(titleBar)
+  if (localStorage.getItem('jwt') == null) {
+    $('.login').show();
+  } else {
+    $('.login').hide();
     
-    var articleBar = $('<div class="feed-bar"><div class="feed-bar__item feed-bar__up" onclick="scrollUp()"></div><div class="feed-bar__item feed-bar__down" onclick="scrollDown()"></div><div class="feed-bar__item feed-bar__close" onclick="closeArticle()"></div></div>')
-    
-    $.each(feedItems, function( index ) {
-      var item = feedItems[index]
-      var itemElement = $('<div class="feed-item" onclick="openArticle()"></div>')
-      var date = item.pubDate || item.published || item.date;
-      date = new Date(date)
-      date = date.toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric', year:'numeric' }) + " " + date.toLocaleTimeString('en-GB', {hour12: 'numeric', minute: 'numeric'})
-      var itemLink = $('<div class="feed-label"><p class="feed-link" href="' + item.link + '">' + item.title +'</p><small class="feed-date">' + date.slice(0, -3) + '</small></div>')
-      
-      var itemContent;
-      if(typeof item.encoded != "undefined"){
-        articleContent = item.encoded;
-      } else if(typeof item.description == "undefined"){
-        articleContent = item.content.content;
-      } else {
-        articleContent = item.description;
-      }
-      
-      var itemContent = $('<div class="feed-content u-hidden" onclick="doNothing()"><h1 class="feed-content__title">' + item.title + '</h1><a href="' + item.link + '">Source</a><br>' + articleContent + '</div>')
-      itemContent.append(articleBar.clone())
-      
-      itemElement.append(itemLink, itemContent)
-      articlesElement.append(itemElement)
-    })
-    textFit(document.getElementsByClassName('feed-label'), {multiLine: true, minFontSize:21, alignVert: true});
+    getFeeds();
+  }
+}
+init();
+
+
+$("#login_button").click(function(){
+  $.ajax({
+    url : base + "auth/login",
+    type: "post",
+    dataType: "json",
+    data: '{"email":"' + $('#email').val() + '","password":"' + $('#pass').val() + '"}',
+    contentType: "application/json"
+  }).done(function(data){
+    window.localStorage.setItem('jwt', data.jwt);
+    window.localStorage.setItem('id', data._id);
+    init();
   })
+});
+
+$("#logout_button").click(function(){
+  window.localStorage.clear();
+  feedsElement.hide();
+  currentFeed = 0;
+  articlesElement.html("");
+  window.location.hash = currentFeed
+  init();
+});
+
+function getFeeds(){
+  $.ajax({
+    url : base + "follows?type=rss",
+    type: "get",
+    dataType: "json",
+    headers: {
+      "authorization": "Bearer " + window.localStorage.jwt
+    },
+    contentType: "application/json"
+  }).done(function(data){
+    
+    if(!window.feeds) window.feeds = [];
+    
+    $.each(data, function( index ) {
+      
+      window.feeds.push({
+        name: data[index].rss.description,
+        id: data[index].rss._id
+      })
+      
+      appendFeed(data[index], index);
+    });
+  }).then(function(){
+    getArticles();
+  });
+}
+
+function appendFeed(item, index) {
+  var feedElement = $('<div class="feed" onclick="loadFeed(\'' + index + '\')">' + item.rss.description +'</div>')
+  feedsElement.append(feedElement)
+}
+
+function getArticles(){
+  $.ajax({
+    url : base + "articles?page=1&per_page=10&rss=" + window.feeds[currentFeed].id + "&sort_by=publicationDate,desc",
+    type: "get",
+    dataType: "json",
+    headers: {
+      "authorization": "Bearer " + window.localStorage.jwt
+    },
+    contentType: "application/json"
+  }).done(function(data){
+    console.log(data);
+    appendArticles(data);
+  })
+}
+
+function appendArticles(items) {
+  articlesElement.html("");
+  window.location.hash = currentFeed
+  
+  var titleBar = $('<div class="feed-title" onclick="toggleMenu()">' + window.feeds[currentFeed].name + '</div>')
+  articlesElement.append(titleBar);
+  
+  $.each(items, function( index ) {
+    var item = items[index]
+    var itemElement = $('<div class="feed-item" onclick="getArticle(\'' + item._id + '\')"></div>')
+    var date = new Date(item.publicationDate)
+    date = date.toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric', year:'numeric' }) + " " + date.toLocaleTimeString('en-GB', {hour12: 'numeric', minute: 'numeric'})
+    var itemName = $('<div class="feed-label"><p class="feed-link">' + item.title +'</p><small class="feed-date">' + date.slice(0, -3) + '</small></div>')
+    
+    itemElement.append(itemName)
+    articlesElement.append(itemElement)
+  })
+}
+
+window.getArticle = function(article_id) {
+  if(loading) return;
+  
+  loading = true;
+  $.ajax({
+    url : base + "articles/" + article_id + "?type=parsed",
+    type: "get",
+    dataType: "json",
+    headers: {
+      "authorization": "Bearer " + window.localStorage.jwt
+    },
+    contentType: "application/json"
+  }).done(function(data){
+    console.log(data);
+    loading = false;
+    appendArticle(data);
+  })
+}
+
+var articleBar = $('<div class="feed-bar"><div class="feed-bar__item feed-bar__up" onclick="scrollUp()"></div><div class="feed-bar__item feed-bar__down" onclick="scrollDown()"></div><div class="feed-bar__item feed-bar__close" onclick="closeArticle()"></div></div>')
+
+function appendArticle(item) {
+  var itemContent = $('<div class="feed-content" onclick="doNothing()"><h1 class="feed-content__title">' + item.title + '</h1><a href="' + item.url + '">Source</a><br>' + item.content + '</div>')
+  itemContent.append(articleBar.clone())
+  
+  articleElement.append(itemContent);
+  articleElement.show();
 }
 
 window.closeArticle = function(){
   event.stopPropagation()
-  var el = findParentWithClass(event.target, "feed-content");
-  el.classList.add("u-hidden");
-}
-
-window.openArticle = function(){
-  event.preventDefault()
-  var el = findParentWithClass(event.target, "feed-item");
-  el.lastChild.classList.toggle("u-hidden");
+  
+  articleElement.hide();
+  articleElement.html("");
 }
 
 window.scrollUp = function(){
   event.preventDefault()
   event.stopPropagation()
-  var el = findParentWithClass(event.target, "feed-content");
-  el.scrollTop -= window.innerHeight * 0.8
+  $(".feed-content")[0].scrollTop -= window.innerHeight * 0.8
 }
 
 window.scrollDown = function(){
   event.preventDefault()
   event.stopPropagation()
-  var el = findParentWithClass(event.target, "feed-content");
-  el.scrollTop += window.innerHeight * 0.8
+  $(".feed-content")[0].scrollTop += window.innerHeight * 0.8
 }
 
 window.doNothing = function(){
   event.stopPropagation()
 }
 
-function findParentWithClass(el, cls) {
-  while ((el = el.parentNode) && el.className.indexOf(cls) < 0);
-  return el;
+window.previousFeed = function(e){
+  currentFeed = (currentFeed - 1)
+  if (currentFeed < 0) currentFeed = window.feeds.length-1
+  getArticles();
+}
+window.nextFeed = function(e){
+  currentFeed = (currentFeed + 1) % window.feeds.length
+  getArticles();
+}
+window.loadFeed = function(id){
+  feedsElement.hide();
+  currentFeed = id
+  getArticles();
+}
+
+$(".backdrop").click(function(e){
+  console.log(e);
+  feedsElement.hide();
+});
+
+window.toggleMenu = function(){
+  feedsElement.toggle();
 }
